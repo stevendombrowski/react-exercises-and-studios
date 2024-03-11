3758295503f3b50f51dd3f3a4d77529d243b86d9
Steven <dombrowskis153@yahoo.com>
2024-03-11 04:38:02 -0400

    completed

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import classes from './ChoresList.module.css';
 export default function ChoresList () {
    return (
+      <div className={classes.background}>
+      <h3 className={classes.choresHeading}>Today's Chores</h3>
+
       <ol>
-         <li>Take out trash</li>
-         <li>Do laundry</li>
-         <li>Wash dog</li>
-         <li></li>
+         <li className={classes.choresText}>Take out trash</li>
+         <li className={classes.choresText}>Do laundry</li>
+         <li className={classes.choresText}>Wash dog</li>
+        
       </ol>
+      </div>
    );
 }
